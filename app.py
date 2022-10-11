43ee561f9ad70e54753af9af85887725a84cf9a6
ashutoshsom1 <ashutoshsomvanshi5@gmail.com>
2022-10-11 21:20:11 +0530

    updating the modules for better performance

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
-from ocr import *
-from PIL import Image
+from typing import List, Any
+
+import easyocr
 
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
+from ocr import *
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads/'
-
+IMAGE_PATH = UPLOAD_FOLDER + 'file'
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -16,16 +19,12 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
-# functions defined in test.py
-
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
-def home(file):
-    pathconfig(file)
+def home():
     return render_template('index.html')
 
 
@@ -41,7 +40,6 @@ def upload_image():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print('upload_image filename: ', filename)
         flash('Image successfully uploaded and displayed below')
         return render_template('index.html', filename=filename)
     else:
@@ -51,9 +49,36 @@ def upload_image():
 
 @app.route('/display/<filename>')
 def display_image(filename):
+    output = check(show_result())
     # print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+    return redirect(url_for('static', filename='uploads/' + filename),data = output, code=301)
 
+
+"""@app.route('/<output>')
+def script_output():
+    output = check(show_result())
+    return render_template('Result.html', output=output)"""
+
+
+"""@app.route('/', methods=['PAST'])
+def show_result():
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext('filename')
+    a1 = []
+    if len(result) == 0:
+        flash('Given file is not invoice')
+    else:
+        for i in range(len(result)):
+            x = result[i][1]
+            a1.append(x)
+        return a1
+
+
+def check(a1):
+    for i, word in zip(range(len(a1)), a1):
+        if word in ["GSTIN", "GST/UIN", "GSTIN No"]:
+            temp = a1[i + 1]
+            return str(temp)"""
 
 if __name__ == "__main__":
     app.run()
--- ocr.py
+++ ocr.py
@@ -1,44 +1,76 @@
+import fileinput
+
 from flask import flash
 import easyocr
+import cv2
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_FOLDER = 'static/uploads/'
+IMAGE_PATH = UPLOAD_FOLDER + 'file'
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+#
 
-"""## 1. Read in images or video"""
-
-
-def pathconfig(file):
-    image = 'static/uploads', file
-    return image
-
-
-def read(image):
+"""def read(filename):
     reader = easyocr.Reader(['en'])
-    result = reader.readtext(image)
+    result = reader.readtext(filename)
     if len(result) == 0:
         flash('Given file is not invoice')
     else:
-        return result
-
-
-def check(result):
+        return result"""
+"""#def remove_clutter(result):
+    a1 = []
     for i in range(len(result)):
+        x = result[i][1]
+        a1.append(x)
+        return a1"""
+"""def check(a1):
+    for i, word in zip(range(len(a1)), a1):
+       if word in ["GSTIN", "GST/UIN", "GSTIN No"]:
+        temp = a1[i + 1]
+        return temp
+"""
+
+"""for i in range(len(result)):
         for j in range(len(result[i])):
-            if result[i][j] == ('GSTIN', 'GST', 'GST/UIN'):
-                if len(result[i+1][j]) == 15:
-                    if type(result[i+1][j] == 'str'):
-                      print('GST : ', result[i + 1][j])
-                    else:
-                      print("GST present is not feasible")
-                else:
-                  print("INVOICE does not have clear GST no.")
-            else:
-              print("INVOICE does not have GST")
+            if result[i][j] in ['GSTIN']:
+                if len(result[i + 1][j]) == 15:
+                    if type(result[i + 1][j] == 'str'):
+                        return result[i + 1][j]"""
 
-
-"""## 2. Draw Results """
-"""def show(IMAGE_PATH):
-  img = cv2.imread(IMAGE_PATH)
-  plt.imshow(img)
+"""## 2. Draw Results"""
+"""def show(filename):
+  img = cv2.imread(filename)
+  plt.imshow()
   plt.show()"""
+
+
+def show_result():
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(IMAGE_PATH)
+    a1 = []
+    if len(result) == 0:
+        flash('Given file is not invoice')
+    else:
+        for i in range(len(result)):
+            x = result[i][1]
+            a1.append(x)
+        return a1
+
+
+def check(a1):
+    for i, word in zip(range(len(a1)), a1):
+        if word in ["GSTIN", "GST/UIN", "GSTIN No"]:
+            temp = a1[i + 1]
+            return str(temp)
+
+
+"""def check(a1):
+    for i, word in zip(range(len(a1)), a1):
+        if word in ["GSTIN", "GST/UIN", "GSTIN No"]:
+            temp = a1[i + 1]
+            return str(temp)"""
